2c8fdf2b8fa48b5fbe4d885093b5c9a4819638db
Nino Annighoefer <nino@ninoan.com>
2024-11-03 20:11:08 +0000

    Fix bug oops

--- js/main.ts
+++ js/main.ts
@@ -5,30 +5,37 @@ class Instruction {
    */
   static decode(data: Uint8Array): [Instruction, Uint8Array] {
     if (data.length === 0) throw new TypeError("No data");
+
+    if ((data[0] & 0b11111100) === 0b10001000) {
+      return Mov_rm_to_from_r.decode(data);
+    }
+
+    throw new Error("Unrecognised opcode");
   }
 }
 
 /**
- * Instruction class corresponding to the MOV mnemonic.
+ * MOV instruction: Memory/register to/from register.
  */
-export class Mov extends Instruction {
+export class Mov_rm_to_from_r extends Instruction {
   /**
    * Assuming `data` begins with a valid MOV instruction, decode that, and
    * return it and the remainder of the machine code. If anything goes wrong,
    * throw.
    */
-  static override decode(data: Uint8Array): [Mov, Uint8Array] {
-    return [new Mov(), data];
+  static override decode(data: Uint8Array): [Mov_rm_to_from_r, Uint8Array] {
+    console.log(data, data.slice(1));
+    return [new Mov_rm_to_from_r(), data.slice(1)];
   }
 }
 
 /**
- * Decode 8086 machine code instructions and return an array of instructions.
+ * Decode 8086 machine code and yield each instruction.
  */
 export function* decode_instructions(data: Uint8Array): Generator<Instruction> {
   let rest = data;
   while (rest.length > 0) {
-    const [instruction, newRest] = Instruction.decode(data);
+    const [instruction, newRest] = Instruction.decode(rest);
     yield instruction;
     rest = newRest;
   }
--- js/main_test.ts
+++ js/main_test.ts
@@ -2,9 +2,6 @@ import { assertEquals } from "@std/assert";
 import { decode_instructions } from "./main.ts";
 import { assemble } from "./assemble.ts";
 
-Deno.test(function otherTest() {
-});
-
 Deno.test(async function mov_single_register() {
   const original = await Deno.readFile(
     "/Users/Nino/code-friends/computer_enhance_sources/perfaware/part1/listing_0037_single_register_mov",
@@ -12,8 +9,4 @@ Deno.test(async function mov_single_register() {
   const asm = Array.from(decode_instructions(original)).join('\n');
   const reassembled = await assemble(asm);
   assertEquals(original, reassembled);
-});
-
-Deno.test(function addTest() {
-  assertEquals(5, 5);
 });
