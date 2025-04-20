ea013e491ee16416da0a484405dc19ea5652e5e8
Nino Annighoefer <nino@ninoan.com>
2025-04-20 22:31:23 +0100

    'almost progress'

--- js/main.ts
+++ js/main.ts
@@ -1,41 +1,105 @@
-class Instruction {
-  /**
-   * Decode one instruction. Return the decoded instruction and the remainder of
-   * the input data, so the rest of the data can be decoded.
-   */
-  static decode(data: Uint8Array): [Instruction, Uint8Array] {
-    if (data.length === 0) throw new TypeError("No data");
+type Operand =
+  | { type: "IMMEDIATE"; value: Uint8Array }
+  | { type: "MEMORY"; value: bigint }
+  | { type: "REGISTER"; name: string };
 
-    if ((data[0] & 0b11111100) === 0b10001000) {
-      return Mov_rm_to_from_r.decode(data);
-    }
+type InstructionName = "MOV" | "ADD";
 
-    throw new Error("Unrecognised opcode");
+type Instruction = {
+  name: InstructionName;
+  leftOperand?: Operand;
+  rightOperand?: Operand;
+};
+
+/**
+ * Decode one instruction. Return the decoded instruction and the remainder of
+ * the input data, so the rest of the data can be decoded.
+ */
+function decodeInstruction(data: Uint8Array): [Instruction, Uint8Array] {
+  if (data.length === 0) throw new TypeError("No data");
+  const flipOperands = parseIsFlipped(data[0]);
+  const instruction = {
+    name: parseInstructionName(data[0]),
+  };
+
+  if ((data[0] & 0b11111100) === 0b10001000) {
+    return decodeMovMemoryOrRegisterToOrFromRegister(data);
+  }
+
+  throw new Error("Unrecognised opcode");
+}
+
+function parseIsFlipped(bits: number): boolean {
+  return (bits & 0b11111110) === 0b10001010 ||
+    (bits & 0b1111111) === 0b00000010;
+}
+
+export function parseInstructionName(bits: number): InstructionName {
+  const isMov = (bits & 0b11111100) === 0b10001000 ||
+    (bits & 0b11111110) === 0b11000110 || (bits & 0b11110000) === 0b10110000;
+  if (isMov) {
+    return "MOV";
+  }
+  throw new Error("Unrecognised opcode");
+}
+
+function parseRegister(bits: number, isWord: boolean): string {
+  const threeBits = bits & 0b111;
+  switch (threeBits) {
+    case 0b000:
+      return isWord ? "AX" : "AL";
+    case 0b001:
+      return isWord ? "CX" : "CL";
+    case 0b010:
+      return isWord ? "DX" : "DL";
+    case 0b011:
+      return isWord ? "BX" : "BL";
+    case 0b100:
+      return isWord ? "SP" : "AH";
+    case 0b101:
+      return isWord ? "BP" : "CH";
+    case 0b110:
+      return isWord ? "SI" : "DH";
+    case 0b111:
+      return isWord ? "DI" : "BH";
+    default:
+      throw new Error("Invalid register bits");
   }
 }
 
-/**
- * MOV instruction: Memory/register to/from register.
- */
-export class Mov_rm_to_from_r extends Instruction {
-  /**
-   * Assuming `data` begins with a valid MOV instruction, decode that, and
-   * return it and the remainder of the machine code. If anything goes wrong,
-   * throw.
-   */
-  static override decode(data: Uint8Array): [Mov_rm_to_from_r, Uint8Array] {
-    console.log(data, data.slice(1));
-    return [new Mov_rm_to_from_r(), data.slice(1)];
+/** MOV instruction: Memory/register to/from register. */
+const parsers = {
+  0b1001000: function decodeMovByteMemoryFromRegister(
+    data: Uint8Array,
+  ): [Instruction, Uint8Array] {
+
+  },
+  0b1001010: function decodeMovByteMemoryToRegister(
+    data: Uint8Array,
+  ): [Instruction, Uint8Array] {
+
+  },
+  0b1001001: function decodeMovWordMemoryFromRegister(
+    data: Uint8Array,
+  ): [Instruction, Uint8Array] {
+
+  },
+  0b1001011: function decodeMovWordMemoryToRegister(
+    data: Uint8Array,
+  ): [Instruction, Uint8Array] {
+
   }
 }
 
 /**
  * Decode 8086 machine code and yield each instruction.
  */
-export function* decode_instructions(data: Uint8Array): Generator<Instruction> {
+export function* decode_instructions(
+  data: Uint8Array,
+): Generator<Instruction> {
   let rest = data;
   while (rest.length > 0) {
-    const [instruction, newRest] = Instruction.decode(rest);
+    const [instruction, newRest] = decodeInstruction(rest);
     yield instruction;
     rest = newRest;
   }
--- js/main_test.ts
+++ js/main_test.ts
@@ -1,12 +1,20 @@
-import { assertEquals } from "@std/assert";
-import { decode_instructions } from "./main.ts";
+import { assertEquals, assertThrows } from "@std/assert";
+import { decode_instructions, parseInstructionName } from "./main.ts";
 import { assemble } from "./assemble.ts";
 
 Deno.test(async function mov_single_register() {
   const original = await Deno.readFile(
     "/Users/Nino/code-friends/computer_enhance_sources/perfaware/part1/listing_0037_single_register_mov",
   );
-  const asm = Array.from(decode_instructions(original)).join('\n');
+  const asm = Array.from(decode_instructions(original)).join("\n");
   const reassembled = await assemble(asm);
   assertEquals(original, reassembled);
+});
+
+Deno.test(function instructionNameMov() {
+  assertEquals(parseInstructionName(0b10001011), "MOV");
+  assertEquals(parseInstructionName(0b10001000), "MOV");
+  assertEquals(parseInstructionName(0b10001001), "MOV");
+  assertEquals(parseInstructionName(0b10001010), "MOV");
+  assertThrows(() => parseInstructionName(0b00000000));
 });
